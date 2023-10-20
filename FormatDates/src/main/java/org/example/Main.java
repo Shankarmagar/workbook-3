bed51296efd212d31b288e345a9d4fe31db87abf
Shankar Ale Magar <shankaralemagar91@gmail.com>
2023-10-20 14:08:42 -0400

    Completed

--- FormatDates/src/main/java/org/example/Main.java
+++ FormatDates/src/main/java/org/example/Main.java
@@ -1,15 +1,13 @@
 package org.example;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.TimeZone;
 
 public class Main {
     public static void main(String[] args) {
 
-        LocalDateTime currentTime = LocalDateTime.now(); //
+        LocalDateTime currentTime = LocalDateTime.now(TimeZone.getTimeZone("GMT").toZoneId()); //
         DateTimeFormatter formattingDate = DateTimeFormatter.ofPattern("MM/dd/yyyy"); //formatting or changing to our output based on String passed as a parameter
 
         String fullDate = currentTime.format(formattingDate);
@@ -20,31 +18,18 @@ public class Main {
         DateTimeFormatter monthInWords = DateTimeFormatter.ofPattern("MMMM dd, yyyy");
         String monInWord = currentTime.format(monthInWords);
 
+        DateTimeFormatter DayInWeek = DateTimeFormatter.ofPattern("EEEE, MMM dd, yyyy hh:mm");
+        String dayInWeekWithMoninWords = currentTime.format(DayInWeek);
 
         System.out.println(fullDate);
         System.out.println(newFormattedDate);
         System.out.println(monInWord);
+        System.out.println(dayInWeekWithMoninWords);
 
-
-        //Showing in GMT format
-        TimeZone t = TimeZone.getTimeZone("GMT");
-
-        Date currentDateForGMT = new Date();
-        t = TimeZone.getTimeZone("GMT");
-
-        SimpleDateFormat GMTFrmt= new SimpleDateFormat("EEEE, MMM dd, yyyy hh:mm");
-        GMTFrmt.setTimeZone(t);
-        System.out.println(GMTFrmt.format(currentDateForGMT));
-
-        //Bonus Parts
-        //DateTimeFormatter bonusFormat = DateTimeFormatter.ofPattern("");
-
-        //Bonus parts My local is New York so getting New York time zone
-        TimeZone timeZone = TimeZone.getTimeZone("America/New_York");
-
-        Date currentDate = new Date();
-        SimpleDateFormat localFormat = new SimpleDateFormat("hh:mm 'on' dd-MMM-yyyy");
-        System.out.println(localFormat.format(currentDate));
-
+        //bonus part
+        LocalDateTime currentTimeForBonus = LocalDateTime.now(); //
+        DateTimeFormatter frmt = DateTimeFormatter.ofPattern("hh:mm 'on' dd-MMM-yyyy");
+        System.out.println(currentTimeForBonus.format(frmt));
     }
+
 }
